7d9990b11e2a6908dfc9e57aa76c5c7d9bd32b09
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2026-04-12 17:13:29 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."nononoexe" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
+
---
+++ home.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "nononoexe";
+  home.homeDirectory = "/home/nononoexe";
+  home.stateVersion = "26.05";
+
+  home.packages = with pkgs; [
+    home-manager
+    texlive.combined.scheme-full
+  ];
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Keisuke ANDO";
+      user.email = "ando_k@iwate-pu.ac.jp";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+}
+
