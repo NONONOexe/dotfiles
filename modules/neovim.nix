93328d54e7170d5bd704b628ea8d4cea697160b5
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2026-04-18 21:56:23 +0900

    :sparkles: Add nixd LSP to neovim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,13 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+    ];
+
+    extraPackages = with pkgs; [
+      nixd
+    ];
+
     extraConfig = ''
       set clipboard=unnamedplus
       vnoremap <C-c> "+y
+    '';
+
+    initLua = ''
+      vim.lsp.config('nixd', {})
+      vim.lsp.enable('nixd')
     '';
   };
 }
