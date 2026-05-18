6cfdced0318b1dcc044f48bf75f3ae9f253c6d87
Keisuke ANDO <ando_k@iwate-pu.ac.jp>
2026-05-18 09:50:12 +0900

    :zap: Configure readline, disable bell and enable history search

--- modules/shell.nix
+++ modules/shell.nix
@@ -5,6 +5,13 @@
     "$HOME/.local/bin"
   ];
 
+  home.file.".inputrc".text = ''
+    set bell-style none
+    set completion-ignore-case on
+    "\e[A": history-search-backward
+    "\e[B": history-search-forward
+  '';
+
   programs.bash = {
     enable = true;
     shellAliases = {
