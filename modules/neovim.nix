5720573f7f5f3d513b84b9f219cb6469ffcb058c
Keisuke ANDO <ando_k@iwate-pu.ac.jp>
2026-05-31 17:29:01 +0900

    :zap: Add replace keybinding with which-key popup to neovim config

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -7,6 +7,7 @@
     
     plugins = with pkgs.vimPlugins; [
       nvim-lspconfig
+      which-key-nvim
     ];
 
     extraPackages = with pkgs; [
@@ -36,6 +37,17 @@
     initLua = ''
       vim.lsp.config('nixd', {})
       vim.lsp.enable('nixd')
+
+      -- which-key: show available keybindings in a popup
+      local wk = require("which-key")
+      wk.setup()
+      wk.add({
+        { "<leader>r", group = "replace" },
+        { "<leader>rr", function()
+	    local left = vim.api.nvim_replace_termcodes("<Left>", true, false, true)
+	    vim.api.nvim_feedkeys(":%s//gc" .. left .. left .. left, "t", true)
+	  end, desc = "Replace in file (confirm)", mode = "n" },
+      })
     '';
   };
 }
